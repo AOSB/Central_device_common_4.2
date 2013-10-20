2d8ebe97b797cd3a5093f6a94150036ee4c6b9c4
Kayant <anthonysanwo@googlemail.com>
2013-10-20 01:11:43 +0100

    Fix merge

--- device.mk
+++ device.mk
@@ -41,12 +41,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.debug.alloc=0 \
 	ro.hwui.disable_scissor_opt=true \
 
-# Support for Browser's saved page feature. This allows
-# for pages saved on previous versions of the OS to be
-# viewed on the current OS.
-PRODUCT_PACKAGES += \
-    libskia_legacy
-
 # wifi props
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
